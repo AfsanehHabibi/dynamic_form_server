e1a504a106ab3e3434c0687e41081a111d2ee652
Afsaneh <a.habibi1394@gmail.com>
2020-05-31 02:02:36 +0430

    Change in api points
    *Data moved to file

--- api/form_id.js
+++ api/form_id.js
@@ -1,6 +1,18 @@
 var express = require("express");
 var router = express.Router();
-router.get("/api/forms/*",function(req,res){
+const forms_descriptors = require('./../asset/forms.json');
+router.post("/api/forms/:id", function (req, res) {
     console.log("forms/id call");
+    res.json({ message: "form submitted!" });
 })
+router.get("/api/forms/:id", function (req, res) {
+    console.log(req);
+    console.debug(forms_descriptors.forms.find(item => {
+        return item.id == req.params.id;
+    }));
+    res.json(forms_descriptors.forms.find(item => {
+        return item.id == req.params.id;
+    }));
+})
+
 module.exports = router;
--- api/forms.js
+++ api/forms.js
@@ -1,6 +1,26 @@
 var express = require("express");
 var router = express.Router();
+let forms_names = {"forms":[
+    {"title":"ali",
+    "id":"1234"
+    },
+    {"title":"mohammad",
+    "id":"2"
+    },
+    {"title":"n",
+    "id":"34"
+    },
+    {"title":"l",
+    "id":"78"
+    },
+    {"title":"ollll",
+    "id":"123"
+    }
+]
+};
+const forms_descriptors = require('./../asset/forms.json');
 router.get("/api/forms",function(req,res){
-    console.log("forms call");
+        res.json(forms_names);
+        console.debug("no prop");
 })
 module.exports = router;
